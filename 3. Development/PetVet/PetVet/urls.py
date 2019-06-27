8158415dfa6900f2b307c3261a7792a06ad85fe4
lpinilla <lauti_nipi123@hotmail.com>
2019-06-27 12:18:42 -0300

    ahora las vistas se cargan desde un html, corregido __pycache__ del gitignore :whale:

--- 3. Development/PetVet/PetVet/urls.py
+++ 3. Development/PetVet/PetVet/urls.py
@@ -1,4 +1,4 @@
-"""PetPet URL Configuration
+"""PetVet URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import home_page
 
 urlpatterns = [
+    path('', home_page),
     path('admin/', admin.site.urls),
 ]
