ce72b9a9fa100c256e09598be94105d05851bc27
lpinilla <lauti_nipi123@hotmail.com>
2019-06-27 12:34:27 -0300

    creado mecanismo de context para poder reutilizar el template html para la pagina home y contact

--- 3. Development/PetVet/PetVet/urls.py
+++ 3. Development/PetVet/PetVet/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home_page
+from .views import home_page, contact_page
 
 urlpatterns = [
     path('', home_page),
+    path('contact/', contact_page),
     path('admin/', admin.site.urls),
 ]
--- 3. Development/PetVet/PetVet/views.py
+++ 3. Development/PetVet/PetVet/views.py
@@ -2,4 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+            "title": "Home Page"
+    }
+    return render(request, "home_page.html", context)
+
+def contact_page(request):
+    context = {
+        "title": "Contact Page"
+    }
+    return render(request, "home_page.html", context)
+
