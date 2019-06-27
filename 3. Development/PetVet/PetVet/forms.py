c9171fbda21d4cce4bf94aed94c0004dd3d12489
lpinilla <lauti_nipi123@hotmail.com>
2019-06-27 13:38:52 -0300

    empezado a trabajar sobre el login con form

---
+++ 3. Development/PetVet/PetVet/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    fullname = forms.CharField(
+            widget=forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                        "id": "log_full_name",
+                        "placeholder": "Your full name"
+                })
+            )
+    password = forms.CharField(
+        widget=forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                        "id": "log_full_name",
+                        "placeholder": "Your password"
+                })
+
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+                attrs={
+                    "class": "form-control",
+                        "id": "log_full_name",
+                        "placeholder": "Your email"
+                })
+
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if not "gmail.com" in email:
+            raise forms.ValidationError("Email has to be gmail")
+        return email
--- 3. Development/PetVet/PetVet/views.py
+++ 3. Development/PetVet/PetVet/views.py
@@ -1,15 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import LoginForm
 
 def home_page(request):
+    login_form = LoginForm(request.POST or None)
     context = {
-            "title": "Home Page"
+            "title": "Home Page",
+            "form" : login_form
     }
-    return render(request, "home_page.html", context)
+    if login_form.is_valid():
+        print(login_form.cleaned_data)
+
+
+    return render(request, "logreg/view.html", context)
 
 def contact_page(request):
     context = {
         "title": "Contact Page"
     }
-    return render(request, "home_page.html", context)
+    return render(request, "logreg/view.html", context)
 
