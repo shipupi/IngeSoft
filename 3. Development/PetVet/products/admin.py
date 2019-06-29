12d8093149c62401c3e728a71393ce3a5079fb8a
lpinilla <lauti_nipi123@hotmail.com>
2019-06-29 15:27:17 -0300

    cambios hechos a los modelos de productos y categorias

--- 3. Development/PetVet/products/admin.py
+++ 3. Development/PetVet/products/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from django.contrib import admin 
-from .models import Product
-from .models import Category
+from .models import Product, Category
 # Register your models here.
 
 admin.site.register(Category)
--- 3. Development/PetVet/products/models.py
+++ 3. Development/PetVet/products/models.py
@@ -2,20 +2,37 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-	name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, db_index=True, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-	def __str__(self):
-		return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+	    return self.name
 
 
 class  Product(models.Model):
-	name = models.CharField(max_length=100)
-	# email = models.EmailField()
-	description = models.CharField(max_length=1000)
-	price =models.IntegerField()
-	stock =models.IntegerField()
-	image =models.ImageField(upload_to="images/")
-	categories = models.ManyToManyField(Category)
+    category = models.ForeignKey(Category, related_name='products',
+            on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, db_index=True)
+    description = models.TextField(blank=True)
+    price =models.DecimalField(max_digits=10,decimal_places=2)
+    available=models.BooleanField(default=True)
+    stock =models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to="images/", blank=True)
 
-	def __str__(self):
-		return self.name
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id','slug'),)
+
+    def __str__(self):
+        return self.name
