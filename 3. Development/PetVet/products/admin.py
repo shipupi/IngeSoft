e33d61c65eb73113b1ab76352586bf0f7a05a4cd
lpinilla <lauti_nipi123@hotmail.com>
2019-06-29 15:43:08 -0300

    agregada categoria y producto a admin de django

--- 3. Development/PetVet/products/admin.py
+++ 3. Development/PetVet/products/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Product, Category
-# Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'stock', 'available', 'created_at',
+            'updated_at']
+    list_filter = ['available', 'created_at', 'updated_at']
+    list_editable = ['price', 'stock', 'available']
+    prepulated_fields = {'slug' : ('name',)}
+
+admin.site.register(Product, ProductAdmin)
