a09c094cec198d4152805f3b4145589199ea1af9
Brian Ail <ail.brianez@gmail.com>
2019-06-30 14:36:27 -0300

    Sacados seeds del run sh para evitar el error

--- 3. Development/PetVet/run.sh
+++ 3. Development/PetVet/run.sh
@@ -3,6 +3,6 @@ rm -f db.sqlite3
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 ./manage.py loaddata auth
-python3 ./manage.py loaddata categories
-python3 ./manage.py loaddata products
+# python3 ./manage.py loaddata categories
+# python3 ./manage.py loaddata products
 python3 manage.py runserver
