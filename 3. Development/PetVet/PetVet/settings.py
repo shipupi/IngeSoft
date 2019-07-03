624f920eade4fc94f92e838ae3055f108d7074e7
Marina <mfuster@itba.edu.ar>
2019-07-03 20:24:05 -0300

    Update database settings

--- 3. Development/PetVet/PetVet/settings.py
+++ 3. Development/PetVet/PetVet/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'PetVet.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'final-db.sqlite3'),
     }
 }
 
